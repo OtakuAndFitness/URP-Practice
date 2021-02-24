1d08edd88de45ce5a700e8304e0f5f3cf13ba1b8
Bowen Tao <bowentao2015@gmail.com>
2021-02-24 15:27:46 +0800

    change to rider ide

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SimpleMove : MonoBehaviour
 {
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.transform.localPosition = new Vector3(10,10,100);
+        speed *= Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * speed);
+        }
     }
 }
