953243acb5f69d80af757c5f2e3f7256bd1ea334
Bowen Tao <bowentao2015@gmail.com>
2021-02-25 11:02:04 +0800

    scene update

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -15,19 +15,19 @@ public class SimpleMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * speed);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * speed);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * speed);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * speed);
         }
